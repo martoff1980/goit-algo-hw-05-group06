4535a505da148438f380cc7a1eb24be3140625dd
martoff <martoff@ukr.net>
2024-05-06 10:34:26 +0300

    release home work 5 from 06.05.2024

--- home_work5_task2.py
+++ home_work5_task2.py
@@ -1,35 +1,31 @@
-def binary_search(arr, x, accuracy=2):
-    print("Release:", arr)
+def binary_search(arr, x):
     low = 0
     high = len(arr) - 1
     mid = 0
     counter = 0
-    max_step = 0
-    round_x = round(x, accuracy)
     while low <= high:
         mid = int((high + low) // 2)
-        max_step = max(max_step, mid)
         counter += 1
-        cur_value = round(arr[mid], accuracy)
-        print("mid=", mid, "cur_value=", cur_value)
+        cur_value = arr[mid]
         # якщо x більше за значення посередині списку, ігноруємо ліву половину
-        if cur_value < round_x:
+        if cur_value < x:
             low = mid + 1
 
         # якщо x менше за значення посередині списку, ігноруємо праву половину
-        elif cur_value > round_x:
+        elif cur_value > x:
             high = mid - 1
 
         # інакше x присутній на позиції і повертаємо його
         else:
-            return (counter, arr[max_step+1])
+            return (counter, arr[mid+1])
 
     # якщо елемент не знайдений
-    return (counter, f"{arr[max_step]}")
+    if mid == len(arr)-1:
+        mid -= 1
+    return (counter, arr[mid+1])
 
 
-arr = [2/19, 3/8, 4/7, 10/15, 40/45]
-x = 0.57
+arr = [1.1, 1.3, 2.5, 3.8, 4.6, 5.9]
 
-result = binary_search(arr, x, 2)
-print(f"Step: {result[0]}, directed to {result[1]}")
+result = binary_search(arr, 6)
+print(result)
